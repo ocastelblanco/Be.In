fbc0e580dbcf940ab828e911edabbacc206dc27b
Oliver Castelblanco <ocastelblanco@gmail.com>
2014-09-25 17:23:37 +0000

    Vuelta al esquema inicial, solo con el plugin de camera, aplicando snnipet de Chris Hjorth

--- js/bein.js
+++ js/bein.js
@@ -1,53 +1,100 @@
-var pictureSource;   // picture source
-var destinationType; // sets the format of returned value
-// Wait for device API libraries to load
-document.addEventListener("deviceready",onDeviceReady,false);
-// device APIs are available
-function onDeviceReady() {
-    console.log("Dispositivo listo");
-    $('#botonesFoto').show();
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
-}
-// Called when a photo is successfully retrieved
-function onPhotoDataSuccess(imageData) {
-  // Uncomment to view the base64-encoded image data
-  // console.log(imageData);
-  // Get image handle
-  var smallImage = document.getElementById('smallImage');
-  // Unhide image elements
-  smallImage.style.display = 'block';
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-}
-// Called when a photo is successfully retrieved
-function onPhotoURISuccess(imageURI) {
-  // Uncomment to view the image file URI
-  // console.log(imageURI);
-  // Get image handle
-  var largeImage = document.getElementById('largeImage');
-  // Unhide image elements
-  largeImage.style.display = 'block';
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  largeImage.src = imageURI;
-}
-// A button will call this functions -------\/
-function capturePhoto() {
-  // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50, destinationType: destinationType.DATA_URL});
-}
-function capturePhotoEdit() {
-  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL});
-}
-function getPhoto(source) {
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50, destinationType: destinationType.FILE_URI, sourceType: source});
-}
-// Called if something bad happens.
-function onFail(message) {
-    console.log('Failed because: ' + message);
-  alert('Failed because: ' + message);
-}
+/**
+ * @author: Chris Hjorth, www.chrishjorth.com
+ */
+var jqmReady = $.Deferred();
+var pgReady = $.Deferred();
+var app = {
+   //Callback for when the app is ready
+   callback: null,
+   // Application Constructor
+   initialize: function(callback) {
+      this.callback = callback;
+      var browser = document.URL.match(/^https?:/);
+      if(browser) {
+        console.log("Is web.");
+        //In case of web we ignore PG but resolve the Deferred Object to trigger initialization
+	 pgReady.resolve();
+      } else {
+        console.log("Is not web.");
+	    this.bindEvents();
+      }
+   }, bindEvents: function() {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+   }, onDeviceReady: function() {
+      // The scope of 'this' is the event, hence we need to use app.
+      app.receivedEvent('deviceready');
+   }, receivedEvent: function(event) {
+      switch(event) {
+        case 'deviceready':
+    	    pgReady.resolve();
+    	    break;
+      }
+   }
+};
+$(document).on("pageinit", function(event, ui) {
+   jqmReady.resolve();
+});
+/**
+ * General initialization.
+ */
+$.when(jqmReady, pgReady).then(function() {
+   //Initialization code here
+   if(app.callback) {
+      app.callback();
+   }
+   console.log("Frameworks ready. Inicia captura.");
+    // Código de ejemplo desde cordova.apache.org
+    var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
+    // Wait for device API libraries to load
+    //document.addEventListener("deviceready",onDeviceReady,false);
+    // device APIs are available
+    //function onDeviceReady() {
+        console.log("Dispositivo listo");
+        $('#botonesFoto').show();
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    //}
+    // Called when a photo is successfully retrieved
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
+      // Get image handle
+      var smallImage = document.getElementById('smallImage');
+      // Unhide image elements
+      smallImage.style.display = 'block';
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+    // Called when a photo is successfully retrieved
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
+      // Get image handle
+      var largeImage = document.getElementById('largeImage');
+      // Unhide image elements
+      largeImage.style.display = 'block';
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      largeImage.src = imageURI;
+    }
+    // A button will call this functions -------\/
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50, destinationType: destinationType.DATA_URL});
+    }
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL});
+    }
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50, destinationType: destinationType.FILE_URI, sourceType: source});
+    }
+    // Called if something bad happens.
+    function onFail(message) {
+        console.log('Failed because: ' + message);
+      alert('Failed because: ' + message);
+    }
+});
