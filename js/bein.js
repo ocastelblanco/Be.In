5c9c890670f3008afbb91b1843b88b2078ea8394
Oliver Castelblanco <ocastelblanco@gmail.com>
2014-09-26 23:38:33 +0000

    Nueva depuracion

--- js/bein.js
+++ js/bein.js
@@ -51,61 +51,35 @@ app.initialize(function() {
    iniciarCamara();
 });
 function iniciarCamara() {
-    // Código de ejemplo desde cordova.apache.org
-    var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value
-    // Wait for device API libraries to load
-    //document.addEventListener("deviceready",onDeviceReady,false);
-    // device APIs are available
-    //function onDeviceReady() {
-        console.log("Dispositivo listo");
-        $('#botonesFoto').show();
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    //}
-    // Called when a photo is successfully retrieved
-    function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64-encoded image data
-      // console.log(imageData);
-      // Get image handle
-      var smallImage = document.getElementById('smallImage');
-      // Unhide image elements
-      smallImage.style.display = 'block';
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
-    // Called when a photo is successfully retrieved
-    function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI
-      // console.log(imageURI);
-      // Get image handle
-      var largeImage = document.getElementById('largeImage');
-      // Unhide image elements
-      largeImage.style.display = 'block';
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      largeImage.src = imageURI;
-    }
-    // A button will call this functions -------\/
-    function capturePhoto() {
-        console.log('Iniciando captura');
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50, destinationType: destinationType.DATA_URL});
-    }
-    function capturePhotoEdit() {
-        console.log('Iniciando captura con edición');
-      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL});
-    }
-    function getPhoto(source) {
-        console.log('Iniciando captura de foto existente');
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50, destinationType: destinationType.FILE_URI, sourceType: source});
-    }
-    // Called if something bad happens.
-    function onFail(message) {
-        console.log('Failed because: ' + message);
-      alert('Failed because: ' + message);
-    }
+    var pictureSource;
+    var destinationType;
+    console.log("Dispositivo listo");
+    $('#botonesFoto').show();
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
+function onPhotoDataSuccess(imageData) {
+    var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+function onPhotoURISuccess(imageURI) {
+    var largeImage = document.getElementById('largeImage');
+    largeImage.style.display = 'block';
+    largeImage.src = imageURI;
+}
+function capturePhoto() {
+    console.log('Iniciando captura');
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50, destinationType: destinationType.DATA_URL});
+}
+function capturePhotoEdit() {
+    console.log('Iniciando captura con edición');
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL});
+}
+function getPhoto(source) {
+    console.log('Iniciando captura de foto existente');
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50, destinationType: destinationType.FILE_URI, sourceType: source});
+}
+function onFail(message) {
+    console.log('Failed because: ' + message);
 }
