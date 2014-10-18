74caf2f453803a6e8221b3aba28721d93911c5e0
Oliver Castelblanco <ocastelblanco@gmail.com>
2014-10-18 00:00:27 +0000

    Adicion de enlace con Firebase que permite visualizar las fotos guardadas

--- js/bein.js
+++ js/bein.js
@@ -1,11 +1,11 @@
 /**
  * @author: Chris Hjorth, www.chrishjorth.com
+ * Modificaciones ocastelblanco@gmail.com
  */
 var jqmReady = $.Deferred();
 var pgReady = $.Deferred();
 var esPG = false;
-var pictureSource;
-var destinationType;
+var pictureSource, destinationType;
 var app = {
     //Callback for when the app is ready
     callback: null,
@@ -39,6 +39,7 @@ var app = {
 $(document).on("pageinit", function(event, ui) {
     $('#salida').append('jQueryMobile inicializado<br>');
     jqmReady.resolve();
+    $(this).off("pageinit");
 });
 $.when(jqmReady, pgReady).then(function() {
     $('#salida').append('Frameworks ready. Inicia captura<br>');
@@ -62,25 +63,35 @@ function onPhotoDataSuccess(imageData) {
     $('#salida').append('Fin de photoData<br>');
     $('#tomarFotoDialogo').popup('close');
     $('#divFotoB').hide();
+    /*
     var smallImage = document.getElementById('smallImage');
     smallImage.style.display = 'block';
     smallImage.src = "data:image/jpeg;base64," + imageData;
+    */
     $('#smallImage').show().attr('src',"data:image/jpeg;base64," + imageData);
+    fotosBD.push({
+        fotoData:   imageData,
+        tipo:       'data'
+    });
 }
 function onPhotoURISuccess(imageURI) {
     $('#salida').append('Fin de photoURI<br>');
+    $('#tomarFotoDialogo').popup('close');
+    $('#divFotoB').hide();
+    /*
     var largeImage = document.getElementById('smallImage');
     largeImage.style.display = 'block';
     largeImage.src = imageURI;
+    */
     $('#smallImage').show().attr('src',imageURI);
+    fotosBD.push({
+        fotoData:   imageURI,
+        tipo:       'uri'
+    });
 }
 function capturePhoto() {
    $('#salida').append('Iniciando captura<br>');
     navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50, destinationType: destinationType.DATA_URL});
-}
-function capturePhotoEdit() {
-    $('#salida').append('Iniciando captura con edición<br>');
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL});
 }
 function getPhoto(source) {
    $('#salida').append('Iniciando captura de foto existente<br>');
@@ -89,6 +100,42 @@ function getPhoto(source) {
 function onFail(message) {
     $('#salida').append('Failed because: ' + message+'<br>');
 }
+var baseDatos = new Firebase("https://bein.firebaseio.com/");
+var fotosBD = baseDatos.child('fotosArmario');
+// La función de captura de carga de nuevas imágenes
+fotosBD.on('value',function(captura){
+    $('#salida').append('Nuevas fotos<br>');
+    var enlace1 = '<a href="#';
+    var enlace2 = '" data-rel="popup" data-position-to="window" data-transition="fade"><img class="popphoto" src="';
+    var enlace3 = '"></a></div>';
+    var popUpModal1 = '<div data-role="popup" id="';
+    var popUpModal2 = '" data-overlay-theme="b" data-theme="b" data-corners="false"><a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Cerrar</a><img class="popphoto" src="';
+    var popUpModal3 = '"></div>';
+    var columna = true;
+    var celda1 = '<div class="ui-block-';
+    var celda2, enlaceTotal, popUpModalTotal;
+    $('#contFotos').html('');
+    $('#popUpModales').html('');
+    captura.forEach(function(cadaFoto){
+        if (columna) {
+            celda2 = 'a">'
+        } else {
+            celda2 = 'b">'
+        }
+        var nombreFoto = cadaFoto.name();
+        var contFoto = cadaFoto.val();
+        var origenFoto = '';
+        if (contFoto.tipo == 'data') {
+            origenFoto = 'data:image/jpeg;base64,';
+        }
+        enlaceTotal = celda1+celda2+enlace1+nombreFoto+enlace2+origenFoto+contFoto.fotoData+enlace3;
+        popUpModalTotal = popUpModal1+nombreFoto+popUpModal2+origenFoto+contFoto.fotoData+popUpModal3;
+        $('#contFotos').append(enlaceTotal);
+        $('#popUpModales').append(popUpModalTotal);
+        $('#popUpModales #'+nombreFoto).popup();
+        columna = !columna;
+    });
+});
 /* */
 $(function(){
     $('[data-role="page"]').on('pagecreate', function(){
